6c73f19afd7eaaa564ef88eda2acc2a2cc5387a9
Cindy <19cindyl@gmail.com>
2023-12-22 14:22:49 -0500

    proj 5: menu with categories of food items that can be filtered

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({categories, filterItems}) => {
 
-  //manually adding each category button 
+  //dynamic approach which is synced to the data
   return (
     <div className='btn-container'>
-      <button className='filter-btn' onClick={()=> filterItems('breakfast')}> breakfast </button>
-      <button className='filter-btn' onClick={()=> filterItems('lunch')}> lunch </button>
-      <button className='filter-btn' onClick={()=> filterItems('shakes')}> shakes </button>
+      {categories.map((category, index)=> {
+        return (
+          <button type='button' className='filter-btn' key={index} onClick={()=> filterItems(category)}> 
+            {category} 
+          </button>
+        )
+      })}
     </div>
-  );
+  )
+
+  //manually adding each category button 
+  // return (
+  //   <div className='btn-container'>
+  //     <button className='filter-btn' onClick={()=> filterItems('all')}> all </button>
+  //     <button className='filter-btn' onClick={()=> filterItems('breakfast')}> breakfast </button>
+  //     <button className='filter-btn' onClick={()=> filterItems('lunch')}> lunch </button>
+  //     <button className='filter-btn' onClick={()=> filterItems('shakes')}> shakes </button>
+  //   </div>
+  // );
 };
 
 export default Categories;
