57cc437062cf7a27642fd63ba4550aee82807b89
Cindy <19cindyl@gmail.com>
2023-12-23 23:20:31 -0500

    proj 12: modal and nav bar that can show and hide, context api, conditional rendering

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,46 @@
 import React, { useState, useContext } from 'react'
+import App from './App';
+
+const AppContext = React.createContext(); 
+
+const AppProvider = ({children}) => {
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false); 
+    const [isModalOpen, setIsModalOpen] = useState(true); 
+
+    const openSideBar = () => {
+        setIsSideBarOpen(true); 
+    }
+
+    const closeSideBar = () => {
+        setIsSideBarOpen(false); 
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true); 
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false); 
+    }
+
+    return (
+        <AppContext.Provider 
+            value={{
+                isModalOpen,
+                isSideBarOpen,
+                openModal, 
+                openSideBar,
+                closeModal,
+                closeSideBar
+            }}
+        > 
+                {children} 
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
